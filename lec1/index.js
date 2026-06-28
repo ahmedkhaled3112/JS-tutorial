823d2ff64fe048ccc4ca1cb0ade430c7d57561f4
ahmedkhaled3112 <ahmedklose3112@gmail.com>
2026-06-28 11:33:06 +0300

    feat: initialize lec1 project structure with HTML, CSS, and basic JavaScript DOM manipulation

---
+++ lec1/index.js
@@ -0,0 +1,17 @@
+// console.log used to print something on the console
+console.log("Hello World");
+
+// alert used to pop up a window with a message 
+window.alert("Hello World");
+
+// This is a comment     
+
+/* 
+ This is a 
+ multiline 
+ comment
+ */
+
+ //change html element's content by using id
+document.getElementById("heading1").innerHTML = "JSTutorial";
+document.getElementById("para1").innerHTML = "Paragraph changed successfully"; 
