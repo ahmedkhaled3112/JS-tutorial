a1a6392d7b0bdbb33b68c869ad08d43a94eaa845
ahmedkhaled3112 <ahmedklose3112@gmail.com>
2026-07-01 05:58:31 +0300

    feat: initialize variable tutorial project with HTML, CSS, and basic JavaScript data type examples

---
+++ lec2-variables/index.js
@@ -0,0 +1,35 @@
+// Varibale : Is a container that store the value 
+//            the variable behaves as if it were the value it contains
+
+// Decleration : The process of creating a variable 
+// let x;
+
+// initialization : The process of assigning value to the variable 
+// x = 10;
+
+let age = 20;
+let gpa = 3.7;
+let isStudent = true;
+let name = "John";
+let city = null;
+let country = undefined;
+
+//  String are always enclosed in double or single quotes
+console.log(`my age = ${age}`);
+console.log(`my gpa = ${gpa}`);
+console.log(`am i a student = ${isStudent}`);
+console.log(`my name = ${name}`);
+console.log(`my city = ${city}`);
+console.log(`my country = ${country}`);
+
+// Data types 
+// 1. Numbers
+console.log(typeof age);
+// 2. String
+console.log(typeof name);
+// 3. Boolean
+console.log(typeof isStudent);
+// 4. Undefined
+console.log(typeof country);
+// 5. null
+console.log(typeof city);
