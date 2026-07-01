6cba9ea4c82ce637f50e39332f00f7865c6d778c
ahmedkhaled3112 <ahmedklose3112@gmail.com>
2026-07-01 07:02:54 +0300

    feat: add tutorial files demonstrating usage of constants to calculate circle circumference

---
+++ lec6-constants/index.js
@@ -0,0 +1,9 @@
+// Constants (const) are containers for storing values that cannot be changed.
+const PI = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the Radius of a Circle");
+radius = Number(radius);
+circumference = 2 * PI * radius;
+console.log("The circumference is: " + circumference);
